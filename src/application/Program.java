9619ab542be5138b2dfa12be742581096cc07d79
Henrique Aguiar <rickdf7@gmail.com>
2021-06-17 12:42:45 -0400

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entites.Department;
+import model.entites.Seller;
 
 public class Program {
 
@@ -8,6 +11,9 @@ public class Program {
 		
 		Department obj = new Department(1, "Books");
 		System.out.println(obj);
+		
+		Seller seller = new Seller(2, "Henrique", new Date(), "rickdf7@gmail", 3000.0, obj);
+		System.out.println(seller);
 
 	}
 
